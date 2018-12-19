241c4710e48109330912eb047775067aaa86a50c
U-DESKTOP-EGONCSB\All User <natthapoldewqu@gmail.com>
2018-12-19 18:59:17 +0700

    close #18 - ทำ Entity ของระบบเก็บข้อมูลลูกค้า

---
+++ backend/src/main/java/com/sut/se61/demo/entity/Gender.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Gender {
+    @Id @GeneratedValue
+    private @NonNull Long genderID;
+    private  String genderName;
+
+    public Long getGenderID() {
+        return genderID;
+    }
+
+    public void setGenderID(Long genderID) {
+        this.genderID = genderID;
+    }
+
+    public String getGenderName() {
+        return genderName;
+    }
+
+    public void setGenderName(String genderName) {
+        this.genderName = genderName;
+    }
+
+    public Gender(String genderName) {
+        this.genderName = genderName;
+    }
+
+}
