1503fe18d6b9bc1c35c97d11f52ce99152b3ac30
sompornic <sompron281040@gmail.com>
2018-12-19 18:39:31 +0700

    close #16 - ทำ Entity ของศูนย์บริการซ้อมรถ

---
+++ backend/src/main/java/com/sut/se61/demo/entity/CarServiceEntity.java
@@ -0,0 +1,47 @@
+package com.example.demo.Entity;
+import javax.persistence.*;
+import lombok.*;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class CarServiceEntity {
+    @Id
+    @SequenceGenerator(name="user_seq",sequenceName="user_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user_seq")
+    @Column(name="TITLE_ID",unique = true, nullable = false)
+    private @NotNull Long carServiceID;
+    private String nameCarService;
+    private Long dataAddress;
+
+    public Long getDataAddress() {
+        return dataAddress;
+    }
+
+    public void setDataAddress(Long dataAddress) {
+        this.dataAddress = dataAddress;
+    }
+
+    public Long getCarServiceID() {
+        return carServiceID;
+    }
+
+    public void setCarServiceID(Long carServiceID) {
+        this.carServiceID = carServiceID;
+    }
+
+    public String getNameCarService() {
+        return nameCarService;
+    }
+
+    public void setNameCarService(String nameCarService) {
+        this.nameCarService = nameCarService;
+    }
+
+    public CarServiceEntity(String nameCarService){this.nameCarService = nameCarService;}
+}
