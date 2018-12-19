df81002adc707ae3b66be96de7d67f812e659f3d
androidohm6416 <sivaroot.sut@gmail.com>
2018-12-19 18:26:13 +0700

    close #14 - ทำ Entity ของ Payment

---
+++ backend/src/main/java/com/sut/se61/demo/entity/Payment.java
@@ -0,0 +1,24 @@
+package com.demo.ohm.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long paymentID;
+
+    public Payment() {
+    }
+
+    public Long getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(Long paymentID) {
+        this.paymentID = paymentID;
+    }
+}
