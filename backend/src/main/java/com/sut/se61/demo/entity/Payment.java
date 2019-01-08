9b2eb7beba8bcce6be97485312090698868bdc0f
sivaroot <sivaroot.sut@gmail.com>
2019-01-08 16:42:43 +0700

    ทำการแก้ไข column และjoin entity Payment แล้ว - close #42

--- backend/src/main/java/com/sut/se61/demo/entity/Payment.java
+++ backend/src/main/java/com/sut/se61/demo/entity/Payment.java
@@ -1,24 +1,45 @@
-package com.demo.ohm.demo.entity;
+package com.sut.se61.demo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
 @Entity
+@NoArgsConstructor
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "PAYMENT_ID")
     private Long paymentID;
 
-    public Payment() {
-    }
+    @Temporal(TemporalType.DATE)
+    private Date payDate;
 
-    public Long getPaymentID() {
-        return paymentID;
-    }
+    @Column(precision = 10, scale = 2)
+    private Double amount;
 
-    public void setPaymentID(Long paymentID) {
-        this.paymentID = paymentID;
+
+    @ManyToOne
+    @JoinColumn(name = "INVOICE_ID")
+    private Invoice invoice;
+
+
+    @ManyToOne
+    @JoinColumn(name = "CUSTOMER_ID")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "EMPLOYEE_ID")
+    private Employee employee;
+
+    public Payment(Date payDate, Double amount, Invoice invoice, Customer customer, Employee employee) {
+        this.payDate = payDate;
+        this.amount = amount;
+        this.invoice = invoice;
+        this.customer = customer;
+        this.employee = employee;
     }
 }
