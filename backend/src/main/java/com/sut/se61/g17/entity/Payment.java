b83723b46be25fcec3dbe31edba453016846d1be
sivaroot <sivaroot.sut@gmail.com>
2019-01-22 20:30:50 +0700

    Update Entity Class Payment close #50

--- backend/src/main/java/com/sut/se61/g17/entity/Payment.java
+++ backend/src/main/java/com/sut/se61/g17/entity/Payment.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -15,31 +16,63 @@ public class Payment {
     @Column(name = "PAYMENT_ID")
     private Long paymentID;
 
+    @NotNull
+    private Double amount;
     @Temporal(TemporalType.DATE)
     private Date payDate;
 
-    @Column(precision = 10, scale = 2)
-    private Double amount;
-
-
-    @ManyToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "INVOICE_ID")
     private Invoice invoice;
-
-
-    @ManyToOne
-    @JoinColumn(name = "CUSTOMER_ID")
-    private Customer customer;
 
     @ManyToOne
     @JoinColumn(name = "EMPLOYEE_ID")
     private Employee employee;
 
-    public Payment(Date payDate, Double amount, Invoice invoice, Customer customer, Employee employee) {
-        this.payDate = payDate;
+    public Payment(@NotNull Double amount, Date payDate, Invoice invoice, Employee employee) {
         this.amount = amount;
+        this.payDate = payDate;
         this.invoice = invoice;
-        this.customer = customer;
+        this.employee = employee;
+    }
+
+    public Long getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(Long paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Date getPayDate() {
+        return payDate;
+    }
+
+    public void setPayDate(Date payDate) {
+        this.payDate = payDate;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
         this.employee = employee;
     }
 }
