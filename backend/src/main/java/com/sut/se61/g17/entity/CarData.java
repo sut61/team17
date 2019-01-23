9eb06b955e0e9297042c19df7c531a16c6907e73
programcpe <zgramm@outlook.com>
2019-01-23 14:56:43 +0700

    close #64 ทำการ update Entity class CarData

--- backend/src/main/java/com/sut/se61/g17/entity/CarData.java
+++ backend/src/main/java/com/sut/se61/g17/entity/CarData.java
@@ -1,35 +1,101 @@
 package com.sut.se61.g17.entity;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NonNull;
 
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-//@Getter @Setter
-@NoArgsConstructor
-@ToString @EqualsAndHashCode
+@Data
+
 public class CarData {
-    @Id 
-    @GeneratedValue
-    private @NonNull Long id;
-    private @NonNull String content;
+    @Id
+    @SequenceGenerator(name="carData_seq",sequenceName="carData_seq")   // ทำให้ id gen ของตัวเองเองไม่แย่งกันใช้
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="carData_seq")
+    @Column(name="carDataID",unique = true, nullable = false)
+    private @NonNull Long carID;
+    private @NonNull String model;
+    private @NonNull String cC;
 
-    public Long getId() {
-        return id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "branchID" ,nullable = false)
+    private BranchCar branchCar;
+
+    @ManyToOne
+    @JoinColumn(name = "colorID" ,nullable = false)
+    private CarColor carColor;
+
+    @ManyToOne
+    @JoinColumn(name = "carTypeID" ,nullable = false)
+    private CarType carType;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "gearTypeID" ,nullable = false)
+    private GearType gearType;
+
+    public CarData() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long getCarID() {
+        return carID;
     }
 
-    public String getContent() {
-        return content;
+    public void setCarID(Long carID) {
+        this.carID = carID;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public BranchCar getBranchCar() {
+        return branchCar;
+    }
+
+    public void setBranchCar(BranchCar branchCar) {
+        this.branchCar = branchCar;
+    }
+
+    public CarColor getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(CarColor carColor) {
+        this.carColor = carColor;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+
+
+
+    public GearType getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(GearType gearType) {
+        this.gearType = gearType;
+    }
+
+    public String getcC() {
+        return cC;
+    }
+
+    public void setcC(String cC) {
+        this.cC = cC;
     }
 }
 
