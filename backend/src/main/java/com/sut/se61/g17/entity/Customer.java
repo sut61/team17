28ac46572e88b1eea86884e314fe259eb8975f67
hellodewwahnjiab <natthapoldewqu@gmail.com>
2019-01-23 15:01:18 +0700

    แก้ไข Customer Entity close #63

--- backend/src/main/java/com/sut/se61/g17/entity/Customer.java
+++ backend/src/main/java/com/sut/se61/g17/entity/Customer.java
@@ -6,32 +6,31 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Date;
+import java.time.LocalDate;
+
 @NoArgsConstructor
 @Data
 @Entity
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "customer_seq",sequenceName = "customer_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "customer_seq")
     @Column(name = "CUSTOMER_ID")
-    private Long customerID;
+    private @NotNull Long customerID;
 
-    @NotNull
-    @Size(min = 3, max = 30)
+     @Size(min = 3, max = 30)
     private String firstName;
 
-    @NotNull
-    @Size(min = 3, max = 30)
+      @Size(min = 3, max = 30)
     private String lastName;
 
-    @NotNull
-    private String idNumber;
+    @Size(min = 13,max = 13)
+      private String idNumber;
 
-    @NotNull
-    private Byte age;
+     private  String email ;
 
-    @Temporal(TemporalType.DATE)
-    private Date birthday;
+
+     private LocalDate birthday;
 
     @NotNull
     @Size(min = 5, max = 20)
@@ -49,21 +48,102 @@ public class Customer {
     @JoinColumn(name = "careerID", nullable = false)
     private Career career;
 
-    public Customer() {
-    }
 
-    public Customer(@NotNull @Size(min = 3, max = 30) String firstName,
-                    @NotNull @Size(min = 3, max = 30) String lastName,
-                    @NotNull String idNumber, @NotNull Byte age, Date birthday,
-                    @NotNull @Size(min = 5, max = 20) String phone, Address address, Gender gender, Career career) {
+    public Customer(@Size(min = 3, max = 30) String firstName,
+                    @Size(min = 3, max = 30) String lastName,
+                    @Size(min = 13, max = 13) String idNumber, String email, LocalDate birthday,
+                    @NotNull @Size(min = 5, max = 20) String phone,
+                    Address address, Gender gender, Career career) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNumber = idNumber;
-        this.age = age;
+        this.email = email;
         this.birthday = birthday;
         this.phone = phone;
         this.address = address;
         this.gender = gender;
         this.career = career;
     }
+
+    public Long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Long customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Career getCareer() {
+        return career;
+    }
+
+    public void setCareer(Career career) {
+        this.career = career;
+    }
 }
+
+
