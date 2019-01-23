65cdc30fde214030775b4c0d6c1620d639187fe9
sompornic <sompron281040@gmail.com>
2019-01-23 17:15:42 +0700

    close #79 add CarserviceEntity

--- backend/src/main/java/com/sut/se61/g17/entity/CarService.java
+++ backend/src/main/java/com/sut/se61/g17/entity/CarService.java
@@ -1,47 +1,37 @@
 package com.sut.se61.g17.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-import lombok.*;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 
+@Data
+// is a Lombok annotation to create all the getters, setters, equals, hash, and toString methods, based on the fields
 @Entity
-@Getter @Setter
 @NoArgsConstructor
-@ToString @EqualsAndHashCode
 public class CarService {
     @Id
-    @SequenceGenerator(name="user_seq",sequenceName="user_seq")
-    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user_seq")
-    @Column(name="TITLE_ID",unique = true, nullable = false)
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @Column(name = "carServiceID", unique = true, nullable = false)
     private @NotNull Long carServiceID;
-    private String nameCarService;
-    private Long dataAddress;
+    private String carServiceName;
 
-    public Long getDataAddress() {
-        return dataAddress;
+    @ManyToOne
+    @JoinColumn(name = "addressID")
+    private Address address;
+
+
+    @ManyToOne
+    @JoinColumn(name = "carserviceType", nullable = false)
+    private CarServiceType carServiceType;
+
+    public CarService(String carServiceName, Address address, CarServiceType carServiceType) {
+        this.carServiceName = carServiceName;
+        this.address = address;
+        this.carServiceType = carServiceType;
     }
 
-    public void setDataAddress(Long dataAddress) {
-        this.dataAddress = dataAddress;
-    }
 
-    public Long getCarServiceID() {
-        return carServiceID;
-    }
-
-    public void setCarServiceID(Long carServiceID) {
-        this.carServiceID = carServiceID;
-    }
-
-    public String getNameCarService() {
-        return nameCarService;
-    }
-
-    public void setNameCarService(String nameCarService) {
-        this.nameCarService = nameCarService;
-    }
-
-    public CarService(String nameCarService){this.nameCarService = nameCarService;}
 }
