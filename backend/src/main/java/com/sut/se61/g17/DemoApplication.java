64b4bc799bc44d0d39796d30cc59c3ca1a01a680
sivaroot <sivaroot.sut@gmail.com>
2019-01-25 20:14:04 +0700

    close #114 EnableScheduling

--- backend/src/main/java/com/sut/se61/g17/DemoApplication.java
+++ backend/src/main/java/com/sut/se61/g17/DemoApplication.java
@@ -3,9 +3,11 @@ package com.sut.se61.g17;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
