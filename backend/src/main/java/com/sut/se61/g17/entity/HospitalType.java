f99b8fe0147aa555a2b31d796f29e4341e0c420c
nanctsak <chantardsak@gmail.com>
2019-01-25 16:27:16 +0700

    close #103 entity HospitalType

---
+++ backend/src/main/java/com/sut/se61/g17/entity/HospitalType.java
@@ -0,0 +1,38 @@
+package com.sut.se61.g17.entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class HospitalType {
+    @Id
+    @SequenceGenerator(name = "hospital_seq", sequenceName = "hospital_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hospital_seq")
+    @Column(name = "typeID", unique = true, nullable = false)
+    private @NotNull Long typeID;
+    private String typeName;
+
+    public HospitalType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Long getTypeID() {
+        return typeID;
+    }
+
+    public void setTypeID(Long typeID) {
+        this.typeID = typeID;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+}
