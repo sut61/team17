5b55ccfe8404f4547005b3d70363d7fb0e1e88c9
montonkp <monton.kp@gmail.com>
2019-02-06 23:19:36 +0700

    close #123 -ทำ Entity Employee ให้เก็บข้อมูลตัวแทนขายประกันได้

--- backend/src/main/java/com/sut/se61/g17/entity/Employee.java
+++ backend/src/main/java/com/sut/se61/g17/entity/Employee.java
@@ -4,6 +4,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 @Data
 // is a Lombok annotation to create all the getters, setters, equals, hash, and toString methods, based on the fields
@@ -14,63 +18,44 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "EMPLOYEE_ID")
     private Long employeeID;
+    @Size(min = 2, max = 20)
+    @NotNull
     private String username;
     private String password;
+    @Size(min = 2, max = 30)
     private String firstName;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
+    @Size(min = 2, max = 30)
     private String lastName;
+    @Size(min = 13, max = 13)
+    @Pattern(regexp = "\\d+")
+    @Column(unique = true)
+    private String idNumber;
+    private  String email ;
+    private LocalDate birthday;
+
+    //    @NotNull
+    @Size(min = 5, max = 20)
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "addressID" /*, nullable = false*/)
+    private Address address;
 
 
     @ManyToOne
     @JoinColumn(name = "GENDER_ID")
     private Gender gender;
-    public Long getEmployeeID() {
-        return employeeID;
-    }
 
-    public void setEmployeeID(Long employeeID) {
-        this.employeeID = employeeID;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
+    public Employee(String username, String password, @Size(min = 2, max = 30) String firstName, @Size(min = 2, max = 30) String lastName, @Size(min = 13, max = 13) String idNumber, String email, LocalDate birthday,@Size(min = 5, max = 20) String phone, Address address, Gender gender) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.email = email;
+        this.birthday = birthday;
+        this.phone = phone;
+        this.address = address;
+        this.gender = gender;
     }
-
-
 }
