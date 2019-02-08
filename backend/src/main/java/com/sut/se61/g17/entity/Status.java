d08b2eed24b6b6f7594aed8444181c3260831640
programcpe <zgramm@outlook.com>
2019-02-08 16:25:10 +0700

    close #154 เพิ่ม Entity Status

---
+++ backend/src/main/java/com/sut/se61/g17/entity/Status.java
@@ -0,0 +1,38 @@
+package com.sut.se61.g17.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Status {
+    @Id
+    @SequenceGenerator(name="status_seq",sequenceName="status_seq")   // ทำให้ id gen ของตัวเองเองไม่แย่งกันใช้
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="status_seq")
+    @Column(name="statusID")
+    private @NonNull
+    Long statusID;
+    private @NonNull
+    String status;
+
+    public Long getStatusID() {
+        return statusID;
+    }
+
+    public void setStatusID(Long statusID) {
+        this.statusID = statusID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
