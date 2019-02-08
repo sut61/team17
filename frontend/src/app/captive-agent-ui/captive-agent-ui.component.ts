029bbba4be0edad1019169cf91dc68441d65fe26
montonkp <monton.kp@gmail.com>
2019-02-08 21:14:33 +0700

    close #125 ทำ UI หน้าตัวแทนขายประกัน

---
+++ frontend/src/app/captive-agent-ui/captive-agent-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-captive-agent-ui',
+  templateUrl: './captive-agent-ui.component.html',
+  styleUrls: ['./captive-agent-ui.component.css']
+})
+export class CaptiveAgentUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
