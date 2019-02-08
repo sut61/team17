5c344b36affe81772d0511c2652bd227586f7afd
sompornic <sompron281040@gmail.com>
2019-02-08 20:01:13 +0700

    close #178 update CarserviceRepository

--- backend/src/main/java/com/sut/se61/g17/repository/CarServiceRepository.java
+++ backend/src/main/java/com/sut/se61/g17/repository/CarServiceRepository.java
@@ -4,7 +4,10 @@ import com.sut.se61.g17.entity.CarService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CarServiceRepository extends JpaRepository<CarService, Long> {
     CarService findByCarServiceName(String carServiceName);
+    List<CarService> findByAddress_ProvinceProvinceID(Long provinceID);
 }
