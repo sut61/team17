45261b196e5e5814dfcec8ba5149fe6b1eafb8d7
montonkp <monton.kp@gmail.com>
2019-02-08 21:37:01 +0700

    close #126 ทำ Service หน้าตัวแทนขายประกัน

---
+++ frontend/src/app/captive-agent-ui/captive-agent.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const  captiveAgentApi = '//localhost:8080/captiveagent/';
+const  genderPath = captiveAgentApi + 'gender';
+const  provincePath = captiveAgentApi + 'province';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CaptiveAgentService {
+
+  authKey = new HttpHeaders({
+    'Authorization': sessionStorage.getItem('InsuranceAuthToken')
+  });
+  constructor(private http: HttpClient) { }
+
+  public getAllgender(): Observable<any> {
+    return this.http.get(genderPath , {headers: this.authKey}      );
+  }
+  public getAllProvince(): Observable<any> {
+    return this.http.get(provincePath, {headers: this.authKey});
+  }
+  public getDistrictByProvince(id: number): Observable<any> {
+    return this.http.get(captiveAgentApi + 'district/' + id, {headers: this.authKey});
+  }
+  public getSubdtrictByDistrict(id: number): Observable<any> {
+    return this.http.get(captiveAgentApi + 'subdistrict/' + id, {headers: this.authKey});
+  }
+  public postCaptiveAgent(object: any, genderID: number, subDistrictID: number, districtID: number, provinceID: number,birthday:string, passwordCheck:string): Observable<any> {
+    return this.http.post(captiveAgentApi + genderID + '/' + subDistrictID + '/' + districtID + '/' + provinceID + '/' + birthday + '/' + passwordCheck,{
+      'username': object.username,
+      'password': object.password,
+      'idNumber': object.idNumber,
+      'firstName': object.firstName,
+      'lastName': object.lastName,
+      'phone': object.phone,
+      'email': object.email,
+      'address': {
+        'address': object.address
+      }
+    }, {headers: this.authKey});
+  }
+}
