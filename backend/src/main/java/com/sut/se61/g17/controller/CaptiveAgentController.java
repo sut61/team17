421e85473814c2759132cb70c4695e4be0c04277
montonkp <monton.kp@gmail.com>
2019-02-10 04:30:26 +0700

    close #214 ทำ การแก้ไข Code ที่เกี่ยวกับ Selenuim Test

--- backend/src/main/java/com/sut/se61/g17/controller/CaptiveAgentController.java
+++ backend/src/main/java/com/sut/se61/g17/controller/CaptiveAgentController.java
@@ -63,19 +63,52 @@ public class CaptiveAgentController {
                                  @PathVariable String birthday,
                                  @PathVariable String passwordCheck
     ) throws Exception {
+        if(employee.getUsername().length()<2 || employee.getUsername().length()>20)
+            throw new Exception("Username length incorrect!(2-20)");
+        if(!employee.getUsername().matches("\\D\\w*"))
+            throw new Exception("Username incorrect!");
+        if(employeeRepository.existsByUsername(employee.getUsername()))
+            throw new Exception("Username already exists");
+
+        if(employee.getIdNumber().length()!=13)
+            throw new Exception("IdNumber length incorrect!(13)");
+        if(!employee.getIdNumber().matches("\\d+"))
+            throw new Exception("IdNumber incorrect!");
+        if(employeeRepository.existsByIdNumber(employee.getIdNumber()))
+            throw new Exception("IdNumber already exists");
+
         if(!passwordCheck.equals(employee.getPassword()))
             throw new Exception("Password and Confirm incorrect!");
+
+        if(employee.getFirstName().length()<2 || employee.getFirstName().length()>30)
+            throw new Exception("FirstName length incorrect!(2-30)");
+        if(!employee.getFirstName().matches("\\D+"))
+            throw new Exception("FirstName incorrect!");
+
+        if(employee.getLastName().length()<2 || employee.getLastName().length()>30)
+            throw new Exception("LastName length incorrect!(2-30)");
+        if(!employee.getLastName().matches("\\D+"))
+            throw new Exception("LastName incorrect!");
+
+        if(!employee.getEmail().matches("\\w+[@]\\w+[.]com"))
+            throw new Exception("Email incorrect!");
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate maxDate = LocalDate.now().minusYears(18);
         LocalDate dateOfBirth = LocalDate.parse(birthday, formatter);
         if(dateOfBirth.isAfter(maxDate))
             throw new Exception("Please selected birthday before 18 years of current!");
-        try{
+
+        if(employee.getPhone().length()<10 || employee.getPhone().length()>10)
+            throw new Exception("Phone length incorrect!(10)");
+        if(!employee.getPhone().matches("[0]\\d*"))
+            throw new Exception("Phone incorrect!");
+
+        try {
             Gender gender = genderRepository.findById(genderID).get();
             SubDistrict subDistrict = subDistrictRepository.findById(subdistrictID).get();
             District district = districtRepository.findById(districtID).get();
             Province province = provinceRepository.findById(provinceID).get();
-
 
 
             address.setAddress(employee.getAddress().getAddress());
